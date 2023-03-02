d733b0903a52d8f23944e3df9e5f81a533862e17
Nathan-Nguyen1 <nathannguyen801@gmail.com>
2023-03-01 20:32:07 -0700

    "making changes to DB compiler script : vs code dected that i am using bash"

--- scripts/database_compiler.py
+++ scripts/database_compiler.py
@@ -2,7 +2,10 @@ import os
 import csv
 import sqlite3
 
-# move to subfolder and open/read the data
-with open('construction_data/question_answer.csv', newline = '') as csvfile:
+# define path to my CSV file
+csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'construction_data', 'question_answer.csv'))
+
+# open and read the CSV file
+with open(csv_path, newline = '') as csvfile:
     reader = csv.DictReader(csvfile)
-    rows = list(readr)
+    rows = list(reader)
