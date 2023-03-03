3625a31ba9c86c1adb72fc285060366e224caccd
Nathan-Nguyen1 <nathannguyen801@gmail.com>
2023-03-02 18:20:47 -0700

    updating DB compiler python script

--- scripts/database_compiler.py
+++ scripts/database_compiler.py
@@ -3,14 +3,11 @@ import csv
 import sqlite3
 import pandas as pd
 
+# establish absolute path to root project : in order to run the script as a main file, the absolute path must be defined...if ran in interactive window, it gets imported as a module and can run 
+cwd = os.path.abspath(os.getcwd())
+
 # define path to my CSV file
-# csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'construction_data', 'question_answer.csv'))
-
-# Get the path to the directory containing the script
-script_dir = os.path.dirname(os.path.realpath(__file__))
-
-# Construct the path to the CSV file relative to the script directory
-csv_path = os.path.join(script_dir, '..', 'construction_data', 'questions_answers.csv')
+csv_path = os.path.join(cwd, 'construction_data', 'question_answer.csv')
 
 # open and read the CSV file
 with open(csv_path, newline = '') as csvfile:
