03abf1306cdc4800697ef85c194d10f62430326d
Nathan-Nguyen1 <nathannguyen801@gmail.com>
2023-03-05 13:53:32 -0700

    added nft logo, and background color, as well as some useful links

--- app.py
+++ app.py
@@ -1,43 +1,33 @@
-from flask import Flask, render_template, session, url_for
+from flask import Flask, render_template
 import sqlite3
 from datetime import datetime, timedelta
 
-# create a new instance of the Flask class
 app = Flask(__name__)
 
-# used to verify 24 hour waiting period
-app.secret_key = 'mysecretkey'
-
-# define function to pull question and answers from database
+# Define the route for the index page
 @app.route('/')
 def index():
-    # check if user has already seen today's question
-    if 'last_question_seen' in session and session['last_question_seen'] == datetime.today().strftime('%Y-%m-%d'):
-        question = None
-    else:
-        # retrieve question from database
-        conn = sqlite3.connect('app_data/questions.db')
-        c = conn.cursor()
-        c.execute('SELECT question FROM questions WHERE id = 1')
-        question = c.fetchone()[0]
-        conn.close()
-
-        # store today's date in session
-        session['last_question_seen'] = datetime.today().strftime('%Y-%m-%d')
-
-    # retrieve answer from database for the previous day
+    # Connect to the database
     conn = sqlite3.connect('app_data/questions.db')
     c = conn.cursor()
-    yesterday = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
-    c.execute(f"SELECT answer FROM questions WHERE id = (SELECT MAX(id) FROM questions) - 1")
-    last_answer = c.fetchone()[0] if c.fetchone() else None
+
+    # Get the current date
+    current_date = datetime.today().strftime('%Y-%m-%d')
+
+    # Get the ID of the next question to display
+    c.execute("SELECT id FROM questions ORDER BY id DESC LIMIT 1")
+    last_question_id = c.fetchone()[0]
+    question_id = (last_question_id % 2) + 1
+
+    # Get the question and answer from the database
+    c.execute(f"SELECT question, answer FROM questions WHERE id = {question_id}")
+    question, answer = c.fetchone()
+
+    # Close the database connection
     conn.close()
 
-    # store last answer seen date in session
-    session['last_answer_seen'] = yesterday
-
-    return render_template('index.html', question=question, last_answer=last_answer, last_answer_seen=session.get('last_answer_seen'))
-
+    # Render the index.html template with the question and answer
+    return render_template('index.html', question=question, answer=answer)
 
 if __name__ == '__main__':
     app.run(debug=True)
